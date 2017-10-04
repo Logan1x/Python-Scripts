c03925643524b54ea9150fb38d7899da7cb08368
harsha7890 <rharsha1997@gmail.com>
2017-10-04 13:30:03 +0530

    email-password-checker.py (#9)

--- password-strength-checker.py
+++ password-strength-checker.py
@@ -1,6 +1,19 @@
-password = raw_input('Enter the password ')
-special = '!@#$%^&*()?'
-if len(password) >= 8 and not password.islower() and not password.isupper() and not password.isalpha() and not password.isdigit() and any((c in special) for c in password):
-    print'password is strong'
-else:
-    print 'Weak Password'
+def checkx():
+    email = raw_input('Enter the email:')
+    if '@' in email and '.' in email:
+        pass1()
+    else :
+        print ('Check you email')
+        checkx()
+
+def pass1():
+    password = raw_input('Enter the password ')
+    special = '!@#$%^&*()?'
+    if len(password) >= 8 and not password.islower() and not password.isupper() and not password.isalpha() and not password.isdigit() and any((c in special) for c in password):
+        print ('Strong Password')
+        return 1
+    else:
+        print ('Weak Password')	
+	pass1()
+
+checkx()
