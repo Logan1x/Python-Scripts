a3d3d3cfe45573eadbc8e9914da94719e38b28f8
Ashiri <ajaoiyanu@gmail.com>
2017-10-07 00:01:05 +0100

    created a dice_simulator.py program

---
+++ dice-simulator.py
@@ -0,0 +1,10 @@
+import random #import the random module.
+
+def roll_dice():
+    #randrange is a function of the random module
+    #it accepts two postional arguments; start and stop
+    print(random.randrange(1,7)) 
+
+
+
+
