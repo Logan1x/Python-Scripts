a1cccdeb1a12c8b96d68a46e57643e87d278d0bc
Iyanu Ajao <ajaoiyanu@gmail.com>
2017-10-26 08:09:13 +0100

    Twitter retweet bot (#44)
    * Added a dice simulator
    
    * Added a twitter retweet bot

---
+++ bin/dice_simulator.py
@@ -0,0 +1,10 @@
+import random  #  random is a python built-in module
+
+
+def roll_dice():
+    dice_side = random.randrange(1, 7)
+    print(dice_side)
+
+
+if __name__ == '__main__':
+    roll_dice()
