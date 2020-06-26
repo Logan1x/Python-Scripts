c336584aed25dfd2fdab82a6bf55db848d035df1
Vishal Naik <59202862+Monsieurvishal@users.noreply.github.com>
2020-06-26 23:30:01 +0530

    Update dice_simulator.py

--- bin/dice_simulator.py
+++ bin/dice_simulator.py
@@ -1,9 +1,13 @@
-import random  # random is a python built-in module
-
+import random 
 
 def roll_dice():
-    dice_side = random.randrange(1, 7)
-    print(dice_side)
+    while True:
+        dice_side = random.randrange(1, 7)
+        print("No on the top of dice rolled is ",dice_side)
+        yes_or_no=input("Do you want to continue y/n    ")
+        if yes_or_no == 'n':
+            print("Over !")
+            break
 
 
 if __name__ == '__main__':
